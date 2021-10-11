25b12a6ee5a2af8a542e6086de7cc9abd7701c81
Stefano <juststep.2000@gmail.com>
2021-10-11 22:08:55 +0200

    Typo error

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const seed = require('../database/seed');
 const User = require('../models/User')
 
 const initialize = async () => {
-    console.log("Inizializzo database, DB_URI: "+process.env.DB_URI)
+    console.log("Initializing database, DB_URI: "+process.env.DB_URI)
     await mongoose.connect(process.env.DB_URI);
     if (global.config.seed) await seed()
 }
