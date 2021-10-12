e84326cfc591a7ab8d71b8c5388bd3caecb8bf1b
Stefano <juststep.2000@gmail.com>
2021-10-12 10:55:20 +0200

    Changed environment variable

--- public/front-office/src/environments/environment.prod.ts
+++ public/front-office/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'http://site202107.tw.cs.unibo.it',
+  //apiUrl: 'http://site202107.tw.cs.unibo.it',
+  apiUrl: 'http://localhost:8000',
   production: true
 };
