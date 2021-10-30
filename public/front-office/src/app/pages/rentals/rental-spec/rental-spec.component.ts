cb283c2139fa00afe36a3f08866b80469a245715
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2021-10-30 14:04:50 +0200

    Completed reviews

--- public/front-office/src/app/pages/rentals/rental-spec/rental-spec.component.ts
+++ public/front-office/src/app/pages/rentals/rental-spec/rental-spec.component.ts
@@ -1,6 +1,8 @@
+import { RentalsService } from './../../../services/rentalsService.service';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { NotificationsService } from '../../../services/notifications.service';
 
 @Component({
   selector: 'app-rental-spec',
@@ -10,12 +12,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class RentalSpecComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
-              public router: Router) { }
+              public router: Router,
+              private rentalsService: RentalsService,
+              private notificationsService: NotificationsService) { }
 
   @ViewChild('reviewModal') public reviewModal: ModalDirective;
 
 
   rental;
+  review = 3;
+  reviewText;
+  selectedItem = null;
 
   ngOnInit(): void {
 
@@ -27,8 +34,19 @@ export class RentalSpecComponent implements OnInit {
 
   }
 
-  openReviewModal() {
+  openReviewModal(item) {
+    this.selectedItem = item;
     this.reviewModal.show()
+  }
+
+  sendReview() {
+    this.rentalsService.publishReview(this.review, this.reviewText, this.selectedItem._id)
+    .then(review => {
+      let item = this.rental.items.filter(item => item._id = this.selectedItem._id);
+      item[0].reviews.push(review);
+      this.reviewModal.hide();
+      this.notificationsService.success();
+    })
   }
 
 }
