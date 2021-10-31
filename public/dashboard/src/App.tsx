29848cd1876de9e14ffd151de96bac22a5d6122c
andrea-corradetti <andreacorradetti28@gmail.com>
2021-10-31 20:15:55 +0100

    Added login form (not yet wired with back end)

--- public/dashboard/src/App.tsx
+++ public/dashboard/src/App.tsx
@@ -2,10 +2,28 @@ import * as React from "react"
 import {
   ChakraProvider,
 } from "@chakra-ui/react"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import LoginForm from "./components/LoginForm"
+import NavItem from "./components/Navbar/NavItem"
+import {ViewIcon} from "@chakra-ui/icons" 
+import Sidebar from "./components/Navbar/Navbar"
+import NavbarItems from "./components/Navbar/SidebarData";
+
 
 export const App = () => (
+  
   <ChakraProvider>
-    <LoginForm/>
+    <Router>
+      <LoginForm/>
+      <Switch>
+      </Switch>
+    </Router>
   </ChakraProvider>
 )
--- public/dashboard/src/components/LoginForm.tsx
+++ public/dashboard/src/components/LoginForm.tsx
@@ -1,43 +1,92 @@
 import { Button } from "@chakra-ui/button";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
-import { Container, Heading, VStack} from "@chakra-ui/layout";
-import React, {useState} from "react";
-
+import { Center, Container, Heading, VStack} from "@chakra-ui/layout";
+import { Formik, Form, Field, useField, ErrorMessage, FormikProps} from 'formik';
+import React, { MutableRefObject, useRef, useState } from "react";
+import * as Yup from "yup"
+import path from 'path'
 
 
 const admin = {
     user: 'test',
-    password: 123,
+    password: '123',
 }
 
 
 export default function LoginForm() {
 
+    const form = useRef<HTMLFormElement>(null) 
+    //const [user, setUser] = useState({name: '', password: ''})
 
 
-    
-    const [user, setUser] = useState('');
-    const [details, setDetails] = useState({name: '', password: ''});
-
-    function submitHandler(e: React.FormEvent) {
+    function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
-        console.log(details)
+        if (form.current == null) 
+            return;
+        let formData = new FormData(form.current);
+        console.log(formData)
+        fetch(path.join(process.env.PUBLIC_URL, "dashboardLogin"), {
+            method: 'POST',
+            body: new FormData(form.current)})
+            .then(res => {
+                localStorage.setItem("token", JSON.stringify(res))
+                return res;
+            })
     }
 
 
     return (
-        <VStack as="form" onSubmit={submitHandler}>
-            <Heading>Login</Heading>
-            <FormControl id={"username"} isRequired>
-                <FormLabel>Username or Email</FormLabel>
-                <Input onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
-            </FormControl>
-            <FormControl id={"password"} isRequired >
-                <FormLabel>Password</FormLabel>
-                <Input type="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-            </FormControl>
-            <Button type="submit" w="full">Login</Button>
-        </VStack>
+        <Center>
+            <Formik
+            initialValues={{
+                username:"",
+                password:"",
+            }}
+            validationSchema={Yup.object({
+                username: Yup.string()
+                .required("Required"),
+                password: Yup.string()
+                .required("Required")
+            })}
+            onSubmit={handleSubmit}
+            >
+                {(formProps: FormikProps<any>) => (
+                    <Form>
+                        <VStack >
+                            <Field name="username">
+                                {({ field, form }: {} ) => (
+                                    <FormControl 
+                                    isInvalid={form.touched.username &&form.errors.username}>
+                                        <FormLabel> Username or Email</FormLabel>
+                                        <Input {...field} id="name" />
+                                        <FormErrorMessage>{form.errors.username}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+                            <Field name="password">
+                                {({ field, form }) => (
+                                    <FormControl 
+                                    isInvalid={form.touched.password &&form.errors.password}>
+                                        <FormLabel>Password</FormLabel>
+                                        <Input {...field} id="password" />
+                                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                                    </FormControl>
+                                )}
+                            </Field>
+                            <Button
+                                mt={4}
+                                colorScheme="teal"
+                                w="full"
+                                isLoading={formProps.isSubmitting}
+                                type="submit"
+                            >
+                                Submit
+                            </Button>
+                        </VStack>
+                    </Form>
+                )}
+            </Formik>
+        </Center>
     )
 }
