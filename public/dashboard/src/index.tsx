6e917103ffc8f142bfb171b42f20f80b754d151d
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2021-12-10 17:37:16 +0100

    Fixed dashboard build

--- public/dashboard/src/index.tsx
+++ public/dashboard/src/index.tsx
@@ -17,7 +17,7 @@ ReactDOM.render(
     <ColorModeScript />
     <ChakraProvider>
 
-      <BrowserRouter>
+      <BrowserRouter basename="/dashboard">
         <Routes>
           <Route path="/" element={<App />}>
             <Route path="/clients" element={<Clients />}/>
