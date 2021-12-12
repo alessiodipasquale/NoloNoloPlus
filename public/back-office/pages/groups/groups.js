6b04d9acae4fbad47026c14fc21bf6e5cdc26e66
StefanoColamonaco <juststep.2000@gmail.com>
2021-12-12 12:38:14 +0100

    Added groups page to back-office

---
+++ public/back-office/pages/groups/groups.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    getGroups()
+    .done(res => {
+        console.log(res);
+        for(const elem of res){
+            addElemToTable(elem);
+        }
+    });
+});
+
+function addElemToTable(elem) {
+    console.log(elem);
+    var row = $('<tr id='+elem._id+'></tr>');
+    var row1 = $('<td></td>').text(elem._id);
+    var row2 = $('<td></td>').text(elem.name);
+    var row3 = $('<td></td>');
+    
+    /*var openBtn = $('<button type="button" class="btn btn-primary" id='+elem._id+'><i class="far fa-eye"></i></button>')
+    openBtn.click(function (elem) {
+        console.log(elem.target.id);
+    })*/
+
+    var editBtn = $('<button disabled type="button" class="btn btn-primary mr-3" id="'+elem._id+'"><i class="fas fa-eye" id="'+elem._id+'"></i></button>')
+    editBtn.click(function (elem) {
+        console.log(elem.target)
+        getGroupById(elem.target.id)
+        .done(group => {
+            console.log(group);
+            $('#inputEditId').val(group._id)
+            $('#editGroupModal').modal('show')
+        })
+    })
+
+    var deleteBtn = $('<button type="button" class="btn btn-danger" id="'+elem._id+'"><i class="far fa-trash-alt" id="'+elem._id+'"></i></button>')
+    deleteBtn.click(function (elem) {
+        var r = confirm("Sei sicuro di voler eliminare?");
+        if (r) {
+            deleteItem(elem.target.id)
+            .done( () => {
+                $('#'+elem.target.id).remove();
+            });
+        }
+    })
+
+    row3.append([/*openBtn,*/ editBtn, deleteBtn]);
+            
+    row.append([row1, row2, row3]);
+    $('tbody').append(row);
+}
+
+function create() {
+
+}
+
+function edit() {
+
+}
